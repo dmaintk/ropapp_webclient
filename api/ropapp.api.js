61297160758c0b2a96e968743e35ea6431a47e61
Damián <arias.damian@gmail.com>
2015-08-06 01:12:48 -0300

    version simulada de login y logout

--- api/ropapp.api.js
+++ api/ropapp.api.js
@@ -1,142 +1,152 @@
-// Extiendo jQuery con esta función
-jQuery.fn.serializeObject = function() {
-  var arrayData, objectData;
-  arrayData = this.serializeArray();
-  objectData = {};
-
-  $.each(arrayData, function() {
-    var value;
-
-    if (this.value != null) {
-      value = this.value;
-    } else {
-      value = '';
-    }
-
-    if (objectData[this.name] != null) {
-      if (!objectData[this.name].push) {
-        objectData[this.name] = [objectData[this.name]];
-      }
-
-      objectData[this.name].push(value);
-    } else {
-      objectData[this.name] = value;
-    }
-  });
-
-  return objectData;
-};
-
-jQuery.isDef = function( val ) {
-    return typeof val != 'undefined' && val != null;
-}
 
 var Ropapp = (function() {
     var debug = true;
     var baseUrl = "http://ropapp.cloudapp.net/";
-    var token = "";  // TODO: escribir esto en una cookie
-    var username = "";  // idem
+    Cookies.defaults = { expires: 7, path: '/' };
 
-    var ajaxCall = function( type, url, data, onSuccess, onError ) {
-        $.ajax({
+    var ajaxCall = function( type, url, data ) {
+        return $.ajax({
             type: type, 
             url: baseUrl + url, 
             data: data, 
-            dataType: 'json',
-            success: function(response) {
-                onSuccess(response);
-            },
-            error: function(response) {
-                onError(response);
-            }
+            dataType: 'json'  // tipo de respuesta
         });
     }
 
-    var post = function( url, data, onSuccess, onError ) {
-        ajaxCall( 'POST', url, data, onSuccess, onError );
+    var post = function( url, data ) {
+        return ajaxCall( 'POST', url, data );
     }
 
-    var get = function( url, onSuccess, onError ) {
-        ajaxCall( 'GET', url, {}, onSuccess, onError );
+    var get = function( url ) {
+        return ajaxCall( 'GET', url, {} );
     }
 
-    var del = function( url, onSuccess, onError ) {
-        ajaxCall( 'DELETE', url, {}, onSuccess, onError );
+    var del = function( url ) {
+        return ajaxCall( 'DELETE', url, {} );
+    }
+
+    var isDef = function( val ) {
+        return typeof val != 'undefined' && val != null;
     }
 
     var callIfDef = function( func, param ) {
         if(debug)
             console.log(param);
 
-        if($.isDef(func))
+        if(isDef(func))
             func(param);
     }
 
     return {  // Interfaz pública
 
+        /*** Autenticación ***/
+
         login: function( credentials, onSuccess, onError ) {
-            post('signin', credentials, 
-                function(response) {
-                    token = response.access_token;
-                    username = credentials.username;
-                    callIfDef(onSuccess, "Login exitoso");
-                }, 
-                function(response) {
-                    callIfDef(onError, "Usuario o contraseña incorrecta");
-                });
+            var request = post('signin', credentials);
+
+            request.done(function(response) {
+                Cookies.set('token', response.access_token);
+                Cookies.set('username', credentials.username);
+                callIfDef(onSuccess, "Login exitoso");
+            });
+
+            request.fail(function(response) {
+                callIfDef(onError, "Usuario o contraseña incorrecta");
+            });
         },
 
         logout: function( onSuccess, onError ) {
-            post('signout', { access_token: token }, 
-                function(response) {
-                    token = "";
-                    username = "";
-                    callIfDef(onSuccess, "Logout exitoso");
-                }, 
-                function(response) {
-                    callIfDef(onError, "El usuario no estaba loggeado");
-                });   
-        },
+            var request = post('signout', { access_token: Cookies.get('token') });
+            
+            request.done(function(response) {
+                Cookies.remove('token');
+                callIfDef(onSuccess, "Logout exitoso");
+            });
 
-        isLoggedIn: function() {
-            return token != "";
+            request.fail(function(response) {
+                callIfDef(onError, "El usuario no estaba loggeado");
+            });   
         },
 
         registerUser: function( formContent, onSuccess, onError ) {
-            post('signup', formContent, 
-                function(response) {
-                    token = response.access_token;
-                    username = formContent.username;
-                    callIfDef(onSuccess, "Usuario creado exitosamente");
-                }, 
-                function(response) {
-                    callIfDef(onError, "No se pudo crear el usuario: "+
-                            response.responseText);
-                });
+            var request = post('signup', formContent);
+            
+            request.done(function(response) {
+                Cookies.set('token', response.access_token);
+                Cookies.set('username', formContent.username);
+                callIfDef(onSuccess, "Usuario creado exitosamente");
+            });
+
+            request.fail(function(response) {
+                callIfDef(onError, "No se pudo crear el usuario: "+
+                        response.responseText);
+            });
         },
 
-        getUserData: function( user, onSuccess, onError ) {
-            get('user/' + ($.isDef(user) ? user : username), 
-                function(response) {
-                    console.log(user);
-                    callIfDef(onSuccess, response);
-                },
-                function(response) {
-                    console.log(user);
-                    callIfDef(onError, "El usuario no existe");
+        /*** Usuarios ***/
+
+        getUserData: function( username, onSuccess, onError ) {
+            var request = get('user/' + username);
+            
+            request.done(function(response) {
+                callIfDef(onSuccess, response);
+            });
+
+            request.fail(function(response) {
+                callIfDef(onError, "El usuario no existe");
+            });
+        },
+
+        removeUser: function( username, onSuccess, onError ) {
+            var request = del('user/' + username + '?access_token=' + Cookies.get('token'));
+            
+            request.done(function(response) {
+                callIfDef(onSuccess, "Usuario eliminado exitosamente");
+            });
+
+            request.fail(function(response) {
+                callIfDef(onError, "No se pudo eliminar el usuario: "+
+                        response.responseText);
+            });
+        },
+
+        /*** Misc ***/
+
+        isLoggedIn: function() {
+            return isDef(Cookies.get('token'));
+        },
+
+        getUsername: function() {
+            var username = Cookies.get('username');
+            return isDef(username) ? username : "";
+        },
+
+        serializeObject: function( obj ) {  // Convierte un objeto HTML en uno de JS
+            var arrayData, objectData;
+            arrayData = obj.serializeArray();
+            objectData = {};
+
+            $.each(arrayData, function() {
+                var value;
+
+                if (obj.value != null) {
+                    value = obj.value;
+                } else {
+                    value = '';
                 }
-            );
-        },
 
-        removeUser: function( user, onSuccess, onError ) {
-            del('user/' + /*($.isDef(user) ? user : username)*/"lalobla5" + '?access_token=' + token,
-                function(response) {
-                    callIfDef(onSuccess, "Usuario eliminado exitosamente");
-                }, 
-                function(response) {
-                    callIfDef(onError, "No se pudo eliminar el usuario: "+
-                            response.responseText);
-                });
+                if (objectData[obj.name] != null) {
+                    if (!objectData[obj.name].push) {
+                        objectData[obj.name] = [objectData[obj.name]];
+                    }
+
+                    objectData[obj.name].push(value);
+                } else {
+                    objectData[obj.name] = value;
+                }
+            });
+
+            return objectData;
         }
     }
 })();
